9b2bafa8cebd7c8880bdde46866f861adf9e4a97
krajniy <krajniy@gmail.com>
2023-02-09 14:51:30 +0100

    service impl

--- src/main/java/de/telran/calendar/calendar/controller/EventController.java
+++ src/main/java/de/telran/calendar/calendar/controller/EventController.java
@@ -1,5 +1,7 @@
 package de.telran.calendar.calendar.controller;
 
+import de.telran.calendar.calendar.service.EventService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,24 +11,26 @@ import java.util.List;
 @RestController
 public class EventController {
     //TODO date, name, description, location, startTime, endTime
+    @Autowired
+    private EventService eventService;
 
     @GetMapping("/event/{id}")
     public Object getEvent(@PathVariable long id) {
         // Получаем event по id: отправляем GET request на "/event/{id}"
         // с id в path variable,
         // и получаем объект event, если он существует
-        return null;
+        return eventService.get(id);
     }
-
-    @GetMapping("/event")
-    public Object getEvent(@RequestParam LocalDate date) {
-        return null;
-    }
-
-    @GetMapping("/event")
-    public List<Object> getEvent(@RequestParam String name) {
-        return null;
-    }
+//
+//    @GetMapping("/event")
+//    public Object getEvent(@RequestParam LocalDate date) {
+//        return eventService.get(date);
+//    }
+//
+//    @GetMapping("/event")
+//    public List<Object> getEvent(@RequestParam String name) {
+//        return eventService.get(name);
+//    }
 
 
     @GetMapping("/events")
@@ -36,13 +40,14 @@ public class EventController {
         // Отправляем GET запрос на "/events" либо с датой, либо с именем в качестве requestParam,
         // получаем list ивентов, соответствующих запросу
 
-        return null;
+        return eventService.get(date, name);
     }
 
     @PutMapping("/event/{id}")
     public void updateEvent(@PathVariable long id, @RequestBody Object event) {
         // Обновляем ивент: отправляем PUT request на "/event/{id}"
         // где id передается в path variable а обновляемое событие передается как объект
+        eventService.update(id, event);
     }
 
     @PostMapping("/event")
@@ -51,6 +56,7 @@ public class EventController {
         // Создаем новый ивент: отправляем POST request на "/event"
         // вместе с объектом типа event в теле запроса и получаем
         // ответ 201 Created если ивент успешно создан
+        eventService.create(event);
 
     }
 
@@ -59,6 +65,7 @@ public class EventController {
     public void deleteEvent(@PathVariable long id) {
         // Удаляем ивент: отправляем DELETE request на "/event/{id}"
         // передаем id в path variable, получаем 204 No Content response, если ивент успешно удален
+        eventService.delete(id);
 
     }
 
@@ -69,7 +76,7 @@ public class EventController {
         // фильтр ивентов передается как object в теле запроса,
         // получаем list ивентов которые соответствуют фильтру в запросе
 
-        return null;
+        return eventService.getByFilter(filter);
     }
 
 
